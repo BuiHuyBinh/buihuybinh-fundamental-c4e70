f3dc98354be8f159e46caead40f1457503eb6291
BuiHuyBinh <laiyi123star@gmail.com>
2020-02-26 16:47:09 +0700

    initial commit

---
+++ Session03/homeworkss3.js
@@ -0,0 +1,61 @@
+// 1. Variable swap
+// let a = 10
+// let b = 20
+// let tmp = a
+// a = b
+// b = tmp
+// console.log(a, b)
+
+
+// 2. Split String into Array
+// let str = 'Hello beauty there'
+// let res = str.split(" ")
+// console.log(res)
+
+
+// 3. 
+// let a = [4, 5, 7, -8]
+// console.log(...a)
+
+
+// 4.
+// let loop = true
+// while(loop){
+//     let a = prompt("Hi there, welcome to shop admin panel, what do you want (C,R,U,D")
+//     if(a === 'R'){
+//         let a = ['Jeans','T-shirt','Socks']
+//         console.log(a)
+//         break
+//     // }   else if (a === 'U'){
+//         // console.log("This command is not supported")
+//     }   
+//         else if (a === 'C'){
+//         let add = prompt("Enter the new item")
+//         a.push(add)
+//         alert("DONE")
+//     }   else if (a === 'U'){
+//         let edit = Number(prompt("Enter the position you want to update"))
+//         let b = prompt("Enter the new name")
+//         a[edit] = b
+//         alert("DONE")
+//     }   else if (a === D){
+//         let c = Number(prompt("Enter the position you want to delete"))
+//         a.splice(c,1)
+//         alert("DONE")
+//     }
+// }       
+
+// 5.
+
+// let input = prompt("nhap vao di:")
+// let s = input.split(",")
+// console.log(s)
+// let sum = 0 
+// for(let i = 0;i<s.length;i++)
+// {
+//     sum+=Number(s[i])
+// }
+// console.log(`tong la: ${sum}`)
+
+
+// 6.
